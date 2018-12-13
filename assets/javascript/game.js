8e76504a9fd58a9aea69312b0c231427c5a24742
Ericcentrik <enaiman1@gmail.com>
2018-12-13 17:51:21 -0500

    added commits for clarity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,8 +15,7 @@ $(document).ready(function(){
     function newNumber() {
         
         // cpu picks random number (19 and 120)
-        // specialNumber = Math.floor(Math.random()*110 + 19);
-        specialNumber = Math.floor((Math.random()*110)+19)
+        specialNumber = Math.floor((Math.random()*120)+19)
         // console.log (specialNumber);
 
         // setting random value for gems (between 1 - 12)
@@ -28,7 +27,7 @@ $(document).ready(function(){
         // console.log(blue, clear, green, red);
         
     }
-
+// when a new game starts - special number and gem randomly picks a new number
     function newGame () {
         newNumber();
         totalScore = 0;
@@ -41,13 +40,14 @@ $(document).ready(function(){
         
 
     }
-
+    // if you win - win column will increments by one and total score goes back to 0
     function youWin() {
         wins++;
         $("#wins").text(wins);
         totalScore = 0;
         newGame();
     }
+    // if you lose - column will increments by one and total score goes back to 0
     function youLose() {
         losses++;
         $("#losses").text(losses);
@@ -56,35 +56,40 @@ $(document).ready(function(){
     }
 
     newGame();
-
+    // ever time the mouse button hovers over the gem it will fade (due to opacity)
     $(".gemImg").hover(function() {
         $(this).css({opacity: 0.3});
     },
+    // when you move the mouse off, it goes back to its original opacity
         function() {
             $(this).css({opacity: 1});
         });
     
     
-    
+    // when you click the gem store the value as a string
     $(".gemImg").on("click", function() {
         var gemValue = $(this).attr("data-gemvalue");
-        console.log($(this));
-        gemValue = parseInt(gemValue);
-        console.log(typeof gemValue);
         
-
+        // the string is then converted to a number
+        gemValue = parseInt(gemValue);
+        
+        
+        // score depends on the random gemValue
         totalScore = totalScore + gemValue;
-        console.log(totalScore);
         $("#totalScore").text(totalScore);
         
+        // if else statement to dictate if you win or lose
+        // to win score has to equal the random computer generated number
         if (totalScore === specialNumber) {
             youWin();
+            // if the total score is greater then the computer generate number, you lose
         } else if (totalScore > specialNumber) {
             youLose();
         }
     });
 
-    // 
+    // when button is clicked the instructions disapper and game board appears
+    // when button is click the word "start" changes to "restart"
     $(".btn").on("click", function() {
         console.log("button clicked")
         newGame();
